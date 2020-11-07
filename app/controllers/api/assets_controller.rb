91e53d59f35a88340060e4ac64638da7ac8b1ac4
David Lewis <lewidavi@med.umich.edu>
2020-11-06 23:02:15 -0700

    add show method and beging to outline private section

--- app/controllers/api/assets_controller.rb
+++ app/controllers/api/assets_controller.rb
@@ -5,5 +5,17 @@ module Api
 
       render json: @assets
     end
+
+    def show 
+      @asset = Asset.find(params[:id])
+
+      render json: @asset
+    end
+
+    private
+
+    def asset_params
+    end
+
   end
 end
