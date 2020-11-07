d4b4a6b98b59f8a386b63996e09faea9ea6ddef6
David Lewis <lewidavi@med.umich.edu>
2020-11-06 21:36:08 -0700

    declare custom api routes to match technical requirements provided in the readme prompt

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    resources :assets, path: 'list', only: :index
+    resources :assets, path: 'create', only: :create
+    resources :assets, path: 'read', only: :show
+    resources :assets, path: 'modify', only: :update
+    resources :assets, path: 'remove', only: :destroy
+  end
 end
