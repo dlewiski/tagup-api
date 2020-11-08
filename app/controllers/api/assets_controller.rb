662091321c8982c83a169c136db4c868da0815d1
David Lewis <lewidavi@med.umich.edu>
2020-11-08 09:30:45 -0700

    Add comments to assets controller

--- app/controllers/api/assets_controller.rb
+++ app/controllers/api/assets_controller.rb
@@ -1,11 +1,17 @@
 module Api
   class AssetsController < ApplicationController
+    # /api/list
+    # Return all the assets
+    # GET
     def index 
       @assets = Asset.all
 
       render json: @assets
     end
 
+    # /api/read/:id
+    # return assest based upon id provided
+    # GET
     def show 
       @asset = Asset.find(params[:id])
 
